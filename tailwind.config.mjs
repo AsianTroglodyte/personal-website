1288eaca99a02af822b6bc43c78df3cb2d9f87b0
Karan Swansi <kswansi2002@gmail.com>
2024-08-03 08:39:59 -0400

    finished and decied layout method for feed and added embla for carousel functionality

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,49 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: '#1fb6ff',
+				PWBlack: '#141414',
+				PWWhite: "#F0F0F0",
+				PWOlive: "#BCCABC",
+				PWRed: {
+					500: "#CE4B6D",
+					600: "#C8375D",
+					700: "#8F2743"
+				},
+				PWBlue: {
+					500: "#37ADC8",
+					600: "#2498B3",
+				},
+				PWYellow: {
+					400: "#FFD71A",
+					500: "#e9c41a",
+					600: "#E6BD00"
+				},
+				PWGreen: {
+					400: "#7ACD86",
+					500: "#6BC779",
+					600: "#6BC779"
+				}
+			},
+			fontFamily: {
+				notoSerifDisplay: ["Noto Serif Display"],
+				notoSerif: ["Noto Serif"],
+			},
+			// Source for scroller: https://www.youtube.com/watch?v=iLmBy-HKIAw. Current implementation requires duplication of elements
+			// consider implementing version without duplication, using javascript
+			animation: {
+				hori_scroll_L2R: "hori_scroll_L2R 40s linear infinite",
+				hori_scroll_R2L: "hori_scroll_R2L 40s linear infinite",
+			},
+			keyframes: {
+				/* rem for accounting for gap between in beginning and ending states states. Derived by zooming in (yes, really)*/
+				hori_scroll_L2R: {
+					"0%":{transform: "translateX(calc(-50% - 5.5px))"},
+					"100%": {transform: "translateX(0%)"}
+				},
+				hori_scroll_R2L: {
+					"0%": {transform: "translateX(0%)"},
+					"100%": {transform: "translateX(calc(-50% - 5.5px))"}
+				}
 			}
 		},
 	},

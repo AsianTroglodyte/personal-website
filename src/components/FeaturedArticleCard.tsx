267bf7bfe0caa2342dc355dee4c1ac26b4e0db27
Karan Swansi <kswansi2002@gmail.com>
2024-08-20 19:52:15 -0400

    added accesibility features for articles cards, namely focus. Added styling for hover and focus as well.

--- src/components/FeaturedArticleCard.tsx
+++ src/components/FeaturedArticleCard.tsx
@@ -14,29 +14,23 @@ interface UserProps {
 
 
 const FeaturedArticleCard = ( {carouselDatum}: UserProps) => {
-    const {id, title, description, src, alt, tags} = carouselDatum;
     // find the amount of lines to clamp.
-
-
-    // NOTE THIS IS IMPORTANT CONTEXT FOR THE PAGE: the following commented out code 
-    // is a dynamic attempt at height-based line-clamps it is not feasible likely because
-    // getting size data of elements almost directly after mount when the browser determines 
-    // the sizes instead of it being hardcoded is very unreliable and we don't get the end product 
-    // of browser size calculations. A slight delay among other things would help, but it is a 
-    // tremendous time sink to figure it out and it introduces much complexity
-    // (IMPORTANT) global.css contains the css 
-    // that takes care of the height-based clamps.
-    // it is not elegant but there are no serious issues with it at all. 
 
     // Nice interface :D
     const descriptionTextRef = useRef<HTMLParagraphElement>(null);
 
+    const lineNum = -1;
+
     // typically useLayoutEffect may be used, but it is so subtle that I went for more performance with useEffect.
+    // useEffect changes number of allowable lines using clamp based on size of container for description
     useEffect (() => {
         // null checking to satisfy TypeScript
-        // NOTE: size-containment must be set to size for some reason idk why tbh
         if (descriptionTextRef.current !== null && 
-            descriptionTextRef.current.offsetHeight !== 0){
+            descriptionTextRef.current.offsetHeight !== 0){            
+            
+            // when rerunning code by saving, the number of description lines decrease. the following declaration prevents that.
+            // I suspect that it might have to do with height that is acquired by the code while the page is rendering.
+            descriptionTextRef.current.style.maxHeight=  `120px`;
 
             const textHeight = descriptionTextRef.current.getBoundingClientRect().height;
             console.log("descriptionTextRef: ", descriptionTextRef.current.innerText);
@@ -44,13 +38,14 @@ const FeaturedArticleCard = ( {carouselDatum}: UserProps) => {
             const lineNum = Math.floor(textHeight/(1.25 * 16));
             console.log("lineNum: ", lineNum)
             const newMaxHeight = lineNum * 1.25 * 16; 
+            console.log("newMaxHeight", newMaxHeight);
 
             descriptionTextRef.current.style.webkitLineClamp = `${lineNum}`;
             descriptionTextRef.current.style.display =  "-webkit-box";
             descriptionTextRef.current.style.webkitBoxOrient = "vertical";
             descriptionTextRef.current.style.overflow= "hidden";
 
-            descriptionTextRef.current.style.maxHeight =  `${newMaxHeight}px`;
+            descriptionTextRef.current.style.maxHeight=  `${newMaxHeight}px`;
         }
         // dependency as size changes as browser loads stuff
     }, [descriptionTextRef.current]);
@@ -58,11 +53,12 @@ const FeaturedArticleCard = ( {carouselDatum}: UserProps) => {
 
     return (
     <>
-    <div className="flex justify-center content-center flex-[_0_0_10%] h-[36rem]
+    <article className="flex justify-center content-center flex-[_0_0_10%] h-[36rem]
         embla-slide-number">
         {/* Thumbnail. h & w need to be full as img is a container for the img. This makes object-cover work grid-cols-[minmax(_18rem,18rem)*/}
         {/* title description thingy. Max  */}
-        <div className="grid grid-rows-[minmax(_0rem,_22rem)_minmax(_0rem,_16rem)] grid-cols-[minmax(_20rem,_20rem)] bg-white rounded-md shadow-md hover:shadow-lg font-notoSerif">
+        <div className="grid grid-rows-[minmax(_0rem,_22rem)_minmax(_0rem,_16rem)] grid-cols-[minmax(_20rem,_20rem)] bg-white rounded-md shadow-md 
+        font-notoSerif ">
             <img className="object-cover h-full w-full rounded-t-md" alt={carouselDatum.alt} src={carouselDatum.src}></img>
             <div className="flex flex-col p-4 justify-between">
                 <div className="flex flex-col grow gap-3 overflow-hidden">
@@ -89,7 +85,7 @@ const FeaturedArticleCard = ( {carouselDatum}: UserProps) => {
         {/* <style>
         {css}
         </style> */}
-    </div>
+    </article>
     </>
     )
 }
